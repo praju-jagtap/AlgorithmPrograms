e409d0a54ea01cf42810e1a6001d9379e3c1bba3
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-09-21 18:03:20 +0530

    [Prajakta] Add: Using Bubble Sort Display Sorting Array

---
+++ DataStructureAlgorithm/BubbleSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    internal class BubbleSort
+    {
+        public void bubbleSort(string filePath)
+        {
+            int count = 0;
+            string data = File.ReadAllText(filePath);
+            string[] words = data.Split(",");
+            int[] arr = new int[words.Length];
+            for (int j = 0; j < words.Length; j++)
+            {
+                arr[j] = Convert.ToInt32(words[j]);
+            }
+            for (int i = 0; i <= words.Length - 2; i++)
+            {
+                for (int j = 0; j <= arr.Length - 2; j++)
+                {
+                    count = count + 1;
+                    if (arr[j] > (arr[j + 1]))
+                    {
+                        int temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("---------------------------\n");
+            Console.WriteLine("After Sorted Array : ");
+            foreach (int a in arr)
+            {
+                Console.Write(a + " ");
+            }
+            Console.WriteLine("\n---------------------------\n");
+        }
+    }
+}
--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -8,7 +8,7 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("1.Using Binary Search To Find Word From WordList\n2.To Print Prime No Between 0-1000\n");
+                Console.WriteLine("1.Using Binary Search To Find Word From WordList\n2.To Print Prime No Between 0-1000\n3.Bubble Sort Algorithm\n");
                 Console.WriteLine("Please Enter Your Choice : \n");
                 Console.WriteLine("---------------------------\n");
                 int option = Convert.ToInt32(Console.ReadLine());
@@ -21,6 +21,10 @@
                     case 2:
                         PrimeNo primeNo = new PrimeNo();
                         primeNo.Primeno();
+                        break;
+                    case 3:
+                        BubbleSort bubblesort = new BubbleSort();
+                        bubblesort.bubbleSort(FILE_PATH);
                         break;
 
                     default:
