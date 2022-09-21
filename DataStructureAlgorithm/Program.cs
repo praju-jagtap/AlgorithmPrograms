38821923d66d2fc809f4860563ab62d182df9515
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-09-21 18:29:25 +0530

    [Prajakta] Add: In Prime No Print Anagram And Palindrome

--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -8,7 +8,7 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("1.Using Binary Search To Find Word From WordList\n2.To Print Prime No Between 0-1000\n3.Bubble Sort Algorithm\n4.Anagram Detection\n");
+                Console.WriteLine("1.Using Binary Search To Find Word From WordList\n2.To Print Prime No Between 0-1000\n3.Bubble Sort Algorithm\n4.Anagram Detection\n5.In Prime No find Palindrome and Anagarm\n");
                 Console.WriteLine("Please Enter Your Choice : \n");
                 Console.WriteLine("---------------------------\n");
                 int option = Convert.ToInt32(Console.ReadLine());
@@ -29,6 +29,12 @@
                     case 4:
                         Anagarm anagarm = new Anagarm();
                         anagarm.Anagramcheck("heart", "earth");
+                        break;
+                    case 5:
+                        PrimeAnagarm primeAnagarm = new PrimeAnagarm();
+                        primeAnagarm.PrimeNumber();
+                        primeAnagarm.Palindrome();
+                        primeAnagarm.AnagramCheck();
                         break;
 
                     default:
