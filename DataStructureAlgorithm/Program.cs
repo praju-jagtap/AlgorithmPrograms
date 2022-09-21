109de10e2559cd5359e1898a33920361824f05c6
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-09-21 17:26:14 +0530

    [Prajakta] Add: Display Welcome Message

---
+++ DataStructureAlgorithm/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructureAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Data Structure Algorithms Problem Statement");
+        }
+    }
+}
