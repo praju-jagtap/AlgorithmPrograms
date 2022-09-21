09c9a649d06f92039493dd2fc1f62bf457afb8f0
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-09-21 17:43:01 +0530

    [Prajakta] Add: To Find Word From WordList

--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -2,9 +2,28 @@
 {
     internal class Program
     {
+        const string FILE_PATH = @"C:\GitRepository\AlgorithmPrograms\DataStructureAlgorithm\Words.txt";
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Data Structure Algorithms Problem Statement");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.Using Binary Search To Find Word From WordList\n");
+                Console.WriteLine("Please Enter Your Choice : \n");
+                Console.WriteLine("---------------------------\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        BinarySearch search = new BinarySearch();
+                        search.Search(FILE_PATH);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+            }
         }
     }
 }
